4ed81c060d4228649fc401825def023f1d5d9d8a
Scott-will <48844810+Scott-will@users.noreply.github.com>
2021-10-24 21:45:20 -0400

    First commit

---
+++ CycleSafe/CycleSafe/CycleSafe/AppShell.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CycleSafe
+{
+    public partial class AppShell : Xamarin.Forms.ContentPage
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ConnectPage), typeof(ConnectPage));
+        }
+
+        async void Connect(object sender, EventArgs e)
+        {
+            try
+            {
+                var route = $"{nameof(ConnectPage)}";
+                await Shell.Current.GoToAsync(route);
+            }
+
+            catch(Exception error)
+            {
+                Console.WriteLine(error);
+            }
+           
+        }
+    }
+}
---
+++ CycleSafe/CycleSafe/CycleSafe/Views/ConnectPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CycleSafe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConnectPage : ContentPage
+    {
+        public ConnectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

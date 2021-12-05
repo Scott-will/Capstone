287e6fed6f0306ddbe33bbf573a2a66f2d56297d
Scott-will <48844810+Scott-will@users.noreply.github.com>
2021-12-05 17:01:47 -0500

    using java :)

---
+++ CycleSafeJava/app/src/main/java/com/example/cyclesafejava/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.cyclesafejava;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment  extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settingsfragment_layout, container, false);
+    }
+
+    public void Connect(View view){
+            BluetoothHandler handler = new BluetoothHandler();
+            try{
+                handler.Initialize();
+            }
+            catch(Exception e){
+
+        }
+
+    }
+}
